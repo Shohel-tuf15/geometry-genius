6752766076ca1dc73698c0713eee3de4094a24e3
Shohel-tuf15 <shohel.tuf15.webdeveloper@gmail.com>
2024-12-31 01:04:55 +0600

    first commit

---
+++ scripts/triangle.js
@@ -0,0 +1,47 @@
+// function triangle calculation
+function calculateTriangleArea() {
+  // calculate triangle base
+  const triangleBase = document.getElementById("triangle-base");
+  const triangleBaseText = triangleBase.value;
+
+  const baseTriangle = parseFloat(triangleBaseText);
+  console.log(typeof baseTriangle);
+
+  //   calculate triangle height
+  const triangleHeight = document.getElementById("triangle-height");
+  const triangleHeightText = triangleHeight.value;
+
+  const heightTriangle = parseFloat(triangleHeightText);
+  console.log(typeof heightTriangle);
+
+  //   calculate triangle area
+  const triangleArea = 0.5 * baseTriangle * heightTriangle;
+  console.log("The area of triangle is:", triangleArea);
+
+  //   Area display in the screen
+  const triangleAreaDisplay = document.getElementById("triangle-area");
+  triangleAreaDisplay.innerText = triangleArea;
+}
+
+// function rectangle area calculation
+function calculateRectangleArea() {
+  // calculate rectangle width
+  const rectangleWidth = document.getElementById("rectangle-width");
+  console.log(rectangleWidth);
+  const rectangleWidthText = rectangleWidth.value;
+  const widthRectangle = parseFloat(rectangleWidthText);
+
+  //   calculate rectangle length
+  const rectangleLength = document.getElementById("rectangle-length");
+  console.log(rectangleLength);
+  const rectangleLengthText = rectangleLength.value;
+  const lengthRectangle = parseFloat(rectangleLengthText);
+
+  //   calculate rectangle area
+  const rectangleArea = widthRectangle * lengthRectangle;
+  console.log("The area of rectangle is:", rectangleArea);
+
+  //   rectangle area dispaly
+  const rectangleAreaDisplay = document.getElementById("rectangle-area");
+  rectangleAreaDisplay.innerText = rectangleArea;
+}
